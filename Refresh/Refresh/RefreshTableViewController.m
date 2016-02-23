579a7a84b83d7751fcdf97fa1272453586e69a76
MAJIAKUN465 <MAJIKAUN465@pingan.com.cn>
2016-02-23 22:13:07 +0800

    remove file location

--- Refresh/Refresh/RefreshTableViewController.m
+++ Refresh/Refresh/RefreshTableViewController.m
@@ -93,8 +93,6 @@
 - (void)loadMore
 {
     static int count = 0;
-  
-    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         if (count <= 2) {
