1eadba5aca2fbb399ba3060e76b9546927c5d732
MAJIAKUN465 <MAJIKAUN465@pingan.com.cn>
2016-02-24 23:53:45 +0800

    delete hello

--- Refresh/Refresh/RefreshCollectionViewController.m
+++ Refresh/Refresh/RefreshCollectionViewController.m
@@ -90,7 +90,6 @@
 {
     static int count = 0;
     
-    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         if (count <= 2) {
